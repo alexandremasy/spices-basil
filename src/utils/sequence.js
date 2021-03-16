8ad578e4dc8bf01392ab4695dce18d70a026af5f
Alexandre Masy <alexandre@infinity-mobile.io>
2021-03-16 13:37:24 +0100

    feat: Add support for batch size for the sequence

--- src/utils/sequence.js
+++ src/utils/sequence.js
@@ -11,12 +11,30 @@ const iterator = (previous, current) => {
   })
 }
 
-export default (fns = []) => {
-  if (!isArray(fns)){
-    return Promise.reject('fns is not an array')
+/**
+ * Run an array of tasks in sequence, without overlap. 
+ * Each task will be called with the arguments passed to when.
+ * sequence(), and each may return a promise or a value.
+ * When all tasks have completed, the returned promise will resolve to an array containing 
+ * the result of each task at the corresponding array position. The returned promise will 
+ * reject when any task throws or returns a rejection.
+ * 
+ * @param {Array<Function>} tasks
+ * @param {Number} size = 1
+ * @returns {Promise}
+ */
+export default async (tasks, size = 1) => {
+  if (!isArray(tasks)){
+    return Promise.reject('tasks is not an array')
   }
 
-  return fns.concat(() => Promise.resolve())
-  .reduce(iterator, Promise.resolve(false))
-  .then(() => results);
+  let position = 0
+  let results = []
+  while (position < tasks.length){
+    const batch = tasks.slice(position, position + size)
+    results = [...results, ...await Promise.all(batch.map(i => i()))]
+    position += size
+  }
+
+  return results
 }
